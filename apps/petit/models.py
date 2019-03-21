7c6146bf2da87c2521e4c370114953286ce621a8
Camila <macaguegi@yahoo.es>
2019-03-21 16:01:35 -0500

    Cambiando letras capitales para Psycopg2

--- apps/petit/models.py
+++ apps/petit/models.py
@@ -8,7 +8,7 @@
 from django.db import models
 
 
-class Commerce(models.Model):
+class commerce(models.Model):
     k_idcommerce = models.IntegerField(primary_key=True)
     n_commerce = models.CharField(max_length=50)
     o_address = models.CharField(max_length=200, blank=True, null=True)
@@ -19,32 +19,32 @@ class Commerce(models.Model):
         db_table = 'commerce'
 
 
-class Order(models.Model):
+class order(models.Model):
     k_idorder = models.IntegerField(primary_key=True)
     f_dateorder = models.DateField()
     n_tokenorder = models.CharField(max_length=100)
     n_tokenpayment = models.CharField(max_length=100, blank=True, null=True)
     n_status = models.CharField(max_length=3)
     v_total = models.IntegerField()
-    k_idcommerce = models.ForeignKey(Commerce, models.DO_NOTHING, db_column='k_idcommerce', blank=True, null=True)
+    k_idcommerce = models.ForeignKey(commerce, models.DO_NOTHING, db_column='k_idcommerce', blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'order'
 
 
-class Orderdetail(models.Model):
+class orderdetail(models.Model):
     k_iddetail = models.IntegerField(primary_key=True)
     q_quantity = models.IntegerField()
     v_subtotal = models.IntegerField()
-    k_idorder = models.ForeignKey(Order, models.DO_NOTHING, db_column='k_idorder', blank=True, null=True)
-    k_idproduct = models.ForeignKey('Product', models.DO_NOTHING, db_column='k_idproduct', blank=True, null=True)
+    k_idorder = models.ForeignKey(order, models.DO_NOTHING, db_column='k_idorder', blank=True, null=True)
+    k_idproduct = models.ForeignKey('product', models.DO_NOTHING, db_column='k_idproduct', blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'orderdetail'
 
-class Product(models.Model):
+class product(models.Model):
     k_idproduct = models.IntegerField(primary_key=True)
     n_product = models.CharField(max_length=50)
     n_description = models.CharField(max_length=200, blank=True, null=True)
--- apps/petit/views.py
+++ apps/petit/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from django.utils.timezone import now
-from apps.petit.models import Product, Commerce , Order, Orderdetail
+from apps.petit.models import product, commerce , order, orderdetail
 from apps.petit.util import obtenerIP, obtenerFechaExpiracion
 from apps.petit.apiTPaga import ApiTPaga
 from django.contrib import messages
@@ -13,7 +13,7 @@ def inicio(request):
 
 
 def listarProductos(request):
-        productos = Product.objects.all()
+        productos = product.objects.all()
         return render(
         request, 'petit/productos.html', {"productos": productos})
 
@@ -23,16 +23,16 @@ def listarProductos(request):
 def hacerOrden(request):
         print("Hola soy Miguelo en python")
         if request.method == 'POST':
-                order_id = Order.objects.count() + 1
+                order_id = order.objects.count() + 1
                 n_product = 'Collar GPS'
                 v_price = 100
                 arr_items = {'name': n_product,
                                 'value': v_price}
                 total_amount = 100
                 k_idcommerce = 1
-                terminal_id = Commerce.objects.filter(k_idcommerce=k_idcommerce).values_list('n_commerce', flat=True)
+                terminal_id = commerce.objects.filter(k_idcommerce=k_idcommerce).values_list('n_commerce', flat=True)
                 commerce = [entry for entry in terminal_id]
-                #terminal_id = Commerce.objects.filter(k_idcommerce=k_idcommerce).values('n_commerce')
+                #terminal_id = commerce.objects.filter(k_idcommerce=k_idcommerce).values('n_commerce')
                 ip_address = obtenerIP(request)
                 expiration_date = obtenerFechaExpiracion()
                 print(str(expiration_date))
@@ -49,17 +49,17 @@ def hacerOrden(request):
                         )
                         return redirect('/petit/productos')
 
-                OrderObj, created = Order.objects.create(k_idorder=order_id, f_dateorder=now(
+                OrderObj, created = order.objects.create(k_idorder=order_id, f_dateorder=now(
                 ), n_tokenorder=response['idempotency_token'], n_status='CRE', v_total=total_amount, k_idcommerce=k_idcommerce)
                 if created:
                         OrderObj.save()
-                        detail_id = OrderDetail.objects.count() + 1
+                        detail_id = orderdetail.objects.count() + 1
                         quantity = int(request.POST['cantidad'])
-                        OrderDetailObj, createdDetail = OrderDetail.objects.create(
+                        orderdetailObj, createdDetail = orderdetail.objects.create(
                         k_iddetail=detail_id, q_quantity=quantity, v_subtotal=total_amount, k_idorder=order_id)
 
                         if createdDetail:
-                                OrderDetailObj.save()
+                                orderdetailObj.save()
 
                 return redirect('/petit/pagar_productos/'+str(order_id))
         else:
