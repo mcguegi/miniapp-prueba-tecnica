55bfae708367e1be517c68d37bf61bcc478ba4e1
Camila <macaguegi@yahoo.es>
2019-03-24 00:27:37 -0500

    Modificando settings.py

--- minicomercio/settings.py
+++ minicomercio/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_ROOT = STATIC_DIR
 
 # MEDIA
@@ -165,3 +165,8 @@ except ImportError:
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
